8f40757c05e686a7f3a39ebb333a1efd3a500da0
Nils Haberkamp <haberkamp.n@gmail.com>
2026-09-20 15:33:46 +0200

    Again

--- src/color.rs
+++ src/color.rs
@@ -369,7 +369,7 @@ pub mod status {
 
     pub fn pending(cx: &App) -> Hsla {
         if is_dark(cx) {
-            amber(8, cx)
+            amber(7, cx)
         } else {
             amber(11, cx)
         }
